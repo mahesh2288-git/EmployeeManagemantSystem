0da52e4c6cb18bc0d4e54422f221eb30cb51eb70
mahesh2288-git <maheshkolupula2288@gmail.com>
2026-04-18 16:33:19 +0530

    Delete src/main directory

--- src/main/java/mahesh/employee_management/repository/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package mahesh.employee_management.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import mahesh.employee_management.entity.Employee;
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-
-}
--- src/main/java/mahesh/employee_management/repository/UserRepository.java
+++
@@ -1,6 +0,0 @@
-package mahesh.employee_management.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import mahesh.employee_management.entity.User;
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
